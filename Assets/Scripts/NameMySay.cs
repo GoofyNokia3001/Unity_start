3280d9b08a9030c5de1e4a90add58686ea946edd
Daniils <daniils.miscuks@gmail.com>
2023-04-19 12:21:14 +0300

    Save scenes

---
+++ Assets/Scripts/NameMySay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameMySay : MonoBehaviour {
+	public int frames=0;
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello! you are:"+ frames + " frame");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Frame: " + frames);
+		frames++;
+
+    }
+}
